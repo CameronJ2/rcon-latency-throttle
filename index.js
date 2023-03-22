38a88d4ab7fbd278f92456d9816c27754feda44f
Chris Amori <camori@nge.io>
2023-03-21 21:52:21 -0700

    Refactor main into smaller functions

--- index.js
+++ index.js
@@ -95,20 +95,14 @@ const shouldParseIp = function (playfab, ping) {
 }
 
 /**
- * Main execution
+ * Parses Mordhau.log for player ips, and returns an array of dictionaries { ip, playfab, ping }
+ * @param {object} pingDictionary - dictionary of playfabs to ping { 'DreamsPlayfab': 44 }
+ * @returns [{ 'DreamsPlayfab': 'kj251jk512', ip: '111.111.111.11', ping: 50 }]
  */
-const main = async function () {
-  const rcon = await getRcon()
+const getPlayerInfoList = function (pingDictionary) {
+  const entries = Object.entries(pingDictionary)
 
-  const playerList = await timeProfiler('Rcon player list', function () {
-    return getPlayerList(rcon)
-  })
-
-  const pingDictionary = createPingDictionary(playerList)
-  const playfabs = Object.entries(pingDictionary)
-  // [[playfab, ping], [playfab, ping]]
-
-  const ipPromises = playfabs.map(async function ([playfab, ping]) {
+  const promises = entries.map(async function ([playfab, ping]) {
     if (!shouldParseIp(playfab, ping)) {
       return { ip: cached_playfabToIp[playfab], playfab, ping }
     }
@@ -121,41 +115,62 @@ const main = async function () {
     return { ip, playfab, ping }
   })
 
-  // [{ 'DreamsPlayfab': 'kj251jk512', ip: '111.111.111.11', ping: 50 }]
-  const playerInfoList = await Promise.all(ipPromises)
+  return Promise.all(promises)
+}
 
-  await timeProfiler('Rule adding/deleting', function () {
-    const ipsThrottled = new Set()
+/**
+ * Creates/deletes traffic rules depending on logic for each player
+ * @param {*} playerInfoList - should be in format [{ 'DreamsPlayfab': 'kj251jk512', ip: '111.111.111.11', ping: 50 }]
+ */
+const delayPlayers = function (playerInfoList) {
+  const ipsThrottled = new Set()
 
-    // For each ip, check if their ping is under minimum. If so, create a traffic rule
-    const delayPromises = playerInfoList.map(async function (playerInfo) {
-      const currentDelay = cached_playfabToLastDelay[playerInfo.playfab] ?? 0
-      const delayToAdd =
-        playerInfo.ping > 0
-          ? Math.min(Math.max(MIN_PING - playerInfo.ping, -MAX_DELAY_ADDED), MAX_DELAY_ADDED)
-          : 0
-      const newDelay = Math.max(Math.min(currentDelay + delayToAdd, MIN_PING), 0)
+  // For each ip, check if their ping is under minimum. If so, create a traffic rule
+  const delayPromises = playerInfoList.map(async function (playerInfo) {
+    const currentDelay = cached_playfabToLastDelay[playerInfo.playfab] ?? 0
+    const delayToAdd =
+      playerInfo.ping > 0
+        ? Math.min(Math.max(MIN_PING - playerInfo.ping, -MAX_DELAY_ADDED), MAX_DELAY_ADDED)
+        : 0
+    const newDelay = Math.max(Math.min(currentDelay + delayToAdd, MIN_PING), 0)
 
-      console.log({
-        ip: playerInfo.ip,
-        playfab: playerInfo.playfab,
-        rconPing: playerInfo.ping,
-        currentDelay,
-        newDelay
-      })
-
-      cached_playfabToLastDelay[playerInfo.playfab] = newDelay
-
-      if (newDelay > 0) {
-        await NetworkUtils.addOrChangeRule(playerInfo.ip, newDelay)
-        ipsThrottled.add(playerInfo.ip)
-      } else if (ipsThrottled.has(playerInfo.ip)) {
-        await NetworkUtils.deleteRule(playerInfo.ip)
-        ipsThrottled.delete(playerInfo.ip)
-      }
+    console.log({
+      ip: playerInfo.ip,
+      playfab: playerInfo.playfab,
+      rconPing: playerInfo.ping,
+      currentDelay,
+      newDelay
     })
 
-    return Promise.all(delayPromises)
+    cached_playfabToLastDelay[playerInfo.playfab] = newDelay
+
+    if (newDelay > 0) {
+      await NetworkUtils.addOrChangeRule(playerInfo.ip, newDelay)
+      ipsThrottled.add(playerInfo.ip)
+    } else if (ipsThrottled.has(playerInfo.ip)) {
+      await NetworkUtils.deleteRule(playerInfo.ip)
+      ipsThrottled.delete(playerInfo.ip)
+    }
+  })
+
+  return Promise.all(delayPromises)
+}
+
+/**
+ * Main execution
+ */
+const main = async function () {
+  const rcon = await getRcon()
+
+  const playerList = await timeProfiler('Rcon player list', function () {
+    return getPlayerList(rcon)
+  })
+
+  const pingDictionary = createPingDictionary(playerList)
+  const playerInfoList = await getPlayerInfoList(pingDictionary)
+
+  await timeProfiler('Rule adding/deleting', function () {
+    return delayPlayers(playerInfoList)
   })
 
   console.log('All required players have been throttled')
