22e88a64c68cdcab3f36740a103b8bef652276bd
Chris Amori <camori@nge.io>
2023-04-07 20:17:10 -0700

    Add time profiler for getting ips

--- intervals/ip.js
+++ intervals/ip.js
@@ -1,14 +1,15 @@
 const { getAllPlayfabIps } = require('../utils/network')
+const timeProfiler = require('../utils/timeProfiler')
 let hasProgramTerminated = false
 
 let playfabsToIps = {}
 
-const start = async function (pollRate = 20000) {
+const start = async function (pollRate = 10000) {
   if (hasProgramTerminated) {
     return
   }
 
-  const newPlayfabsToIps = await getAllPlayfabIps()
+  const newPlayfabsToIps = await timeProfiler('Getting IPs', getAllPlayfabIps)
   playfabsToIps = newPlayfabsToIps
 
   setTimeout(start, pollRate)
