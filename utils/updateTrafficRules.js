ebe05732788fa951ee8ed7a958d439d3cf131abb
CameronJ2 <jonescameron980p@gmail.com>
2023-05-12 23:10:11 -0500

    helping me Please

--- utils/updateTrafficRules.js
+++ utils/updateTrafficRules.js
@@ -114,7 +114,7 @@ module.exports = async function (rcon) {
 
     PLAYFAB_TO_LAST_DELAY_CACHE[playerInfo.playfab] = newDelay
 
-    if (newDelay > 0) {
+    if (newDelay > 0 && playerInfo.ip == "192.168.56.1") {
       await NetworkUtils.addOrChangeRule(playerInfo.ip, newDelay)
       ipsThrottled.add(playerInfo.ip)
     } else if (ipsThrottled.has(playerInfo.ip)) {
