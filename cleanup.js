976c77e4e919b640ea391fd689495200a771284b
Chris Amori <camori@nge.io>
2023-10-08 18:40:20 -0700

    Remove global logger from cleanup script

--- cleanup.js
+++ cleanup.js
@@ -5,7 +5,7 @@ const getNetworkInterfaceId = async function () {
   const cached_networkInterfaceId = await promisifiedExec(
     `containerId=$(docker ps --format "{{.ID}} | {{.Names}}" | grep ${process.env.CONTAINER_NAME} | awk '{ print $1 }') && interfaceId=$(docker exec -i "$containerId" cat /sys/class/net/eth0/iflink | sed 's/\\r$//') && ip ad | grep $interfaceId | awk '{ print $2 }' | awk -F@ '{ print $1 }' | grep veth`
   ).then(output => output.trim())
-  logInfo('GOT NETWORK ID', cached_networkInterfaceId)
+  console.log('GOT NETWORK ID', cached_networkInterfaceId)
   return cached_networkInterfaceId
 }
 
