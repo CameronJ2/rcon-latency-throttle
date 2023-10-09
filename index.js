f8f31ab5c8e075418ba42376f114a0ae6891e24c
Chris Amori <camori@nge.io>
2023-10-08 18:43:41 -0700

    Use PST timestamp for logs

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 require('dotenv').config()
 const process = require('node:process')
 const formatISO9075 = require('date-fns/formatISO9075')
+const sub = require('date-fns/sub')
 const { start: startRconChat } = require('./modules/rcon-chat')
 
 const programStart = function () {
   startRconChat()
 }
 
+const getCurrentDateInPST = () => {
+  return sub(new Date(), { hours: 7 })
+}
+
 global.logInfo = (...args) => {
-  console.log(`[${formatISO9075(new Date())}] `, ...args)
+  console.log(`[${formatISO9075(getCurrentDateInPST())}] `, ...args)
 }
 
 global.logError = (...args) => {
-  console.error(`[${formatISO9075(new Date())}] `, ...args)
+  console.error(`[${formatISO9075(getCurrentDateInPST())}] `, ...args)
 }
 
 process.on('uncaughtException', (err, origin) => {
